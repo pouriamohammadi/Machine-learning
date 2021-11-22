3fee3cf030e64c8714d3c4bb36c0a2049eb2c497
pouriamohammadi <91168842+pouriamohammadi@users.noreply.github.com>
2021-11-21 22:52:13 -0700

    Add files via upload

---
+++ SVM/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+printf "Problem 2 (a)\n"
+python3 question2a.py
+printf "Problem 2 (b)\n"
+python3 question2b.py
+printf "Problem 3 (a)\n"
+python3 question3a.py
+printf "Problem 3 (b)\n"
+python3 question3b.py
+printf "Problem 3 (c)\n"
+python3 question3c.py
+printf "Problem 3 (d)\n"
+python3 question3d.py
