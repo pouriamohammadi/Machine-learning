4d7501159e819a41b878065a26ba231b6745a0d0
pouriamohammadi <91168842+pouriamohammadi@users.noreply.github.com>
2021-12-12 20:57:38 -0700

    Update run.sh

--- Neural Networks/run.sh
+++ Neural Networks/run.sh
@@ -7,4 +7,11 @@ printf "2B\n"
 python3 2B.py
 printf "2C\n"
 python3 2C.py
-
+printf "2E_RELU\n"
+python3 2E_RELU.py
+printf "2E_TANH\n"
+python3 2E_TANH.py
+printf "3A\n"
+python3 3A.py
+printf "3B\n"
+python3 3B.py
