ef93e13c2a915a4ca1dfe353e8ab81714c6ec582
pouriamohammadi <91168842+pouriamohammadi@users.noreply.github.com>
2021-12-20 00:02:25 -0700

    Update run.sh

--- Final Project/run.sh
+++ Final Project/run.sh
@@ -1,17 +1,6 @@
 case 
 #!/bin/bash
 
-printf "2A\n"
-python3 2A.py
-printf "2B\n"
-python3 2B.py
-printf "2C\n"
-python3 2C.py
-printf "2E_RELU\n"
-python3 2E_RELU.py
-printf "2E_TANH\n"
-python3 2E_TANH.py
-printf "3A\n"
-python3 3A.py
-printf "3B\n"
-python3 3B.py
+printf "Vemont\n"
+python3 Vermont.py
+
